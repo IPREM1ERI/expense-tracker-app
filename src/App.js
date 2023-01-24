6acead827f373db6f6632cf4ccd411eacde67f08
davidv <davidvazquezgit@gmail.com>
2023-01-24 12:38:47 -0800

    add styling to ExpenseItem component, add place holder data to ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h2>Lets get started</h2>
+      <ExpenseItem />
+
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,6 +1,12 @@
 const ExpenseItem = () => {
   return (
-    <h2>Expense Item</h2>
+    <>
+      <div>Date: March 27th 2021</div>
+      <div>
+        <h2>Title: Car Insurance</h2>
+        <div>Amount: $419.12</div>
+      </div>
+    </>
   )
 }
 
