5e6f78de7c8de9d7d6a6691e865089f8874a736b
davidv <davidvazquezgit@gmail.com>
2023-01-30 12:11:06 -0800

    refactor componentsapp and ExpenseItem components

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,8 +1,16 @@
 
 const ExpenseDate = (props) => {
-  const month = expenseDate.toLocaleString('en-US', {month: 'long'})
-  const year = expenseDate.getFullYear()
-  const day = expenseDate.toLocaleString('en-US', {day: '2-digit'})
+  const month = props.date.toLocaleString('en-US', {month: 'long'})
+  const year = props.date.getFullYear()
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+
+  return (
+  <div>
+    <div>{month}</div>
+    <div>{year}</div>
+    <div>{day}</div>
+  </div>
+  )
 }
 
 export default ExpenseDate
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,24 +1,14 @@
 import "./ExpenseItem.css"
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const expenseDate = new Date(2021, 0, 1);
-  const expenseTitle = 'Car Insurance!';
-  const expenseAmount = 243.12;
-
-  const month = expenseDate.toLocaleString('en-US', {month: 'long'})
-  const year = expenseDate.getFullYear()
-  const day = expenseDate.toLocaleString('en-US', {day: '2-digit'})
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description"> 
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
